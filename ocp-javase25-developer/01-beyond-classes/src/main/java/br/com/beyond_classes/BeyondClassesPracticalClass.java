e15180cf607fff2e9090cdee41a3e99653bd83b5
pssilva <pss1suporte@gmail.com>
2026-09-15 23:25:53 -0300

    feat(dia-03-string): Trabalho em progresso! Códigos de outros contexto: Blocos de Construção Tipos Primitivos e Wrapper

---
+++ ocp-javase25-developer/01-beyond-classes/src/main/java/br/com/beyond_classes/BeyondClassesPracticalClass.java
@@ -0,0 +1,53 @@
+package br.com.beyond_classes;
+
+/**
+ * <p> OBJETIVOS DO EXAME OCP ABORDADOS NESTE CAPÍTULO:
+ * <ul>
+ *     <li> Utilizando Conceitos de Orientação a Objetos em Java
+ *     <ul>
+ *          <li>
+ *              Declarar e instanciar objetos Java, incluindo objetos de classes aninhadas, e explicar o ciclo
+ *              de vida do objeto, incluindo criação, reatribuição de referências e coleta de lixo.
+ *           </li>
+ *          <li>
+ *              Criar classes e registros, e definir e usar campos e métodos de instância e estáticos,
+ *              construtores e inicializadores de instância e estáticos.
+ *           </li>
+ *          <li>
+ *              Compreender escopos de variáveis, aplicar encapsulamento e criar objetos imutáveis.
+ *              Usar inferência de tipo de variável local.
+ *           </li>
+ *          <li>
+ *              Implementar herança, incluindo tipos abstratos e selados, bem como classes de registro.
+ *              Sobrescrever métodos, incluindo os da classe Object. Implementar polimorfismo e diferenciar
+ *              entre tipo de objeto e tipo de referência. Realizar conversão de tipo de referência, identificar
+ *              tipos de objeto usando o operador instanceof e casamento de padrões com o operador instanceof e
+ *              a estrutura switch.
+ *           </li>
+ *          <li>
+ *              Criar e usar interfaces, identificar interfaces funcionais e utilizar métodos de interface privados,
+ *              estáticos e padrão.
+ *           </li>
+ *          <li>
+ *              Criar e usar tipos enum com campos, métodos e construtores.
+ *           </li>
+ *     </ul>
+ *
+ * </ul>
+ *
+ * <p> Para mais detalhes veja em:
+ *
+ * @see <a href="https://github.com/pssilva/oracle-certifications/tree/main/ocp-javase25-developer#t%C3%B3picos-da-certifica%C3%A7%C3%A3o" >Oracle Certifications: OCP Java SE 25 Developer :: Tópicos da Certificação</a>
+ */
+public class BeyondClassesPracticalClass {
+
+    public BeyondClassesPracticalClass() {
+
+    }
+
+    public static void main(String[] args){
+
+
+    }
+
+}
