97c188e4a3fcd0d8631706e86007d554d69073eb
pssilva <pss1suporte@gmail.com>
2026-09-22 22:53:26 -0300

    feat(dia-10-heranca): Trabalho em progresso! Além das Classes > Compreendendo Polimorfismo > Utilizando Conceitos de Orientação a Objetos em Java!

---
+++ ocp-javase25-developer/01-beyond-classes/src/main/java/br/com/beyond_classes/Primate.java
@@ -0,0 +1,14 @@
+package br.com.beyond_classes;
+/**
+ * <p>Código presente no Ebook: <a href="https://a.co/d/0alQOByp" >OCP Oracle® Certified Professional Java® SE 21 Developer</a>
+ * <p> » Capítulo 7 ■ Além das Classes
+ * <p> » » Compreendendo Polimorfismo
+ * </ br>
+ *
+ * <p>Escute o áudio explicativo do propósito da questão na Evidência de Estudo: [TRABALHO EM PROGRESSO]
+ */
+public class Primate {
+    public boolean hasHair() {
+        return true;
+    }
+}
